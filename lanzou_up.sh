eba8843e908b344fbd9e3d0767d3bcd4db47cbdf
xiu2 <54703944+XIU2@users.noreply.github.com>
2020-11-10 09:16:58 +0800

    update

--- lanzou_up.sh
+++ lanzou_up.sh
@@ -28,21 +28,21 @@ INFO="[信息]" && ERROR="[错误]" && TIP="[注意]"
 # 检查是否已登录
 _CHECK_LOGIN() {
 	if [[ "${COOKIE_PHPDISK_INFO}" = "" || "${COOKIE_PHPDISK_INFO}" = "XXX" ]]; then
-		_NOTICE "ERROR" "请指定 Cookie 中 phpdisk_info 的值！[$(date '+%Y/%m/%d %H:%M')]"
+		_NOTICE "ERROR" "请指定 Cookie 中 phpdisk_info 的值！"
 	fi
 	if [[ "${COOKIE_YLOGIN}" = "" || "${COOKIE_YLOGIN}" = "XXX" ]]; then
-		_NOTICE "ERROR" "请指定 Cookie 中 ylogin 的值！[$(date '+%Y/%m/%d %H:%M')]"
+		_NOTICE "ERROR" "请指定 Cookie 中 ylogin 的值！"
 	fi
 
 	HTML_CHECK_LOGIN=$(curl -s --http1.1 -b "ylogin=${COOKIE_YLOGIN};phpdisk_info=${COOKIE_PHPDISK_INFO}" -H "${HEADER_CHECK_LOGIN}" "${URL_ACCOUNT}"|grep "登录")
-	[[ ! -z "${HTML_CHECK_LOGIN}" ]]  && _NOTICE "ERROR" "Cookie 已失效，请更新！[$(date '+%Y/%m/%d %H:%M')]"
+	[[ ! -z "${HTML_CHECK_LOGIN}" ]]  && _NOTICE "ERROR" "Cookie 已失效，请更新！"
 }
 
 # 上传文件
 _UPLOAD() {
-	[[ $(du "${NAME_FILE}"|awk '{print $1}') -gt 100000000 ]] && _NOTICE "ERROR" "${NAME}文件大于 100MB！[$(date '+%Y/%m/%d %H:%M')]"
+	[[ $(du "${NAME_FILE}"|awk '{print $1}') -gt 100000000 ]] && _NOTICE "ERROR" "${NAME}文件大于 100MB！"
 	HTML_UPLOAD=$(curl -s -b "ylogin=${COOKIE_YLOGIN};phpdisk_info=${COOKIE_PHPDISK_INFO}" -H "${URL_UPLOAD}" -F "task=1" -F "id=WU_FILE_0" -F "folder_id=${FOLDER_ID}" -F "name=${NAME}" -F "upload_file=@${NAME_FILE}" "${URL_UPLOAD}"|grep '\\u4e0a\\u4f20\\u6210\\u529f')
-	[[ -z "${HTML_UPLOAD}" ]] && _NOTICE "ERROR" "${NAME}文件上传失败！[$(date '+%Y/%m/%d %H:%M')]"
+	[[ -z "${HTML_UPLOAD}" ]] && _NOTICE "ERROR" "${NAME}文件上传失败！"
 	echo -e "${INFO} 文件上传成功！[$(date '+%Y/%m/%d %H:%M')]"
 }
 
@@ -57,9 +57,9 @@ _NOTICE() {
 	    wget --no-check-certificate -t2 -T5 -4 -U "${UA}" -qO- "http://pushplus.hxtrip.com/customer/push/send?token=${TOKEN}&title=${PARAMETER_1}&content=${PARAMETER_2}"
     fi
     if [[ ${PARAMETER_1} == "INFO" ]]; then
-		echo -e "${INFO} ${PARAMETER_2}"
+		echo -e "${INFO} ${PARAMETER_2} [$(date '+%Y/%m/%d %H:%M')]"
 	else 
-		echo -e "${ERROR} ${PARAMETER_2}"
+		echo -e "${ERROR} ${PARAMETER_2} [$(date '+%Y/%m/%d %H:%M')]"
 	fi
 	exit 1
 }
